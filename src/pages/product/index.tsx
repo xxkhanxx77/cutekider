ad7bd5c7bfd6f049e52724bf001dda4889f12be4
aran <aran@odds.team>
2022-09-01 02:40:52 +0700

    remove unused code

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -4,41 +4,39 @@ import { Card, Grid } from "@mui/material";
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { IProduct } from "../../interface/product";
-import landingSquareImage1 from "/src/image/landing-page/landing-square-1.jpg";
-import landingSquareImage2 from "/src/image/landing-page/landing-square-2.jpg";
 
 const Product = () => {
   interface Props {
     product: any;
     key: number;
   }
-  const items = [
-    {
-      name: "Random Name #1",
-      price: 210,
-      images: landingSquareImage1,
-    },
-    {
-      name: "Random Name #1",
-      price: 210,
-      images: landingSquareImage2,
-    },
-    {
-      name: "Random Name #1",
-      price: 210,
-      images: landingSquareImage2,
-    },
-    {
-      name: "Random Name #1",
-      price: 210,
-      images: landingSquareImage2,
-    },
-    {
-      name: "Random Name #1",
-      price: 210,
-      images: landingSquareImage2,
-    },
-  ];
+  // const items = [
+  //   {
+  //     name: "Random Name #1",
+  //     price: 210,
+  //     images: landingSquareImage1,
+  //   },
+  //   {
+  //     name: "Random Name #1",
+  //     price: 210,
+  //     images: landingSquareImage2,
+  //   },
+  //   {
+  //     name: "Random Name #1",
+  //     price: 210,
+  //     images: landingSquareImage2,
+  //   },
+  //   {
+  //     name: "Random Name #1",
+  //     price: 210,
+  //     images: landingSquareImage2,
+  //   },
+  //   {
+  //     name: "Random Name #1",
+  //     price: 210,
+  //     images: landingSquareImage2,
+  //   },
+  // ];
 
   const Item = (props: Props) => {
     return (
